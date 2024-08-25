46c1c9937b7bd97678c006cf9e188b6aebd7595e
execrated <83084731+execrted@users.noreply.github.com>
2024-08-25 12:57:23 -0600

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,190 +1,72 @@
-// Dependencies
-use terminal_menu::{run, menu, label, scroll, string, submenu, back_button, button, mut_menu, list};
-use zip_extensions::zip_create_from_directory;
-use std::{path::PathBuf, fs::{self, File}, io::{Cursor, Write}};
-use platform_dirs::AppDirs;
-use crx_dl::{ChromeCRXQuery, crx_to_zip};
+import { serve } from "https://deno.land/std@0.170.0/http/mod.ts";
+import { sleep } from "https://deno.land/x/sleep@v1.2.1/mod.ts";
 
-// Constants
-const PROXIES_URL: &str = "https://raw.githubusercontent.com/Stefanuk12/RoProPatcher/master/proxies.txt";
+const DataFetchURL = "https://raw.githubusercontent.com/Stefanuk12/RoProPatcher/proxy/data.json";
+let Data = { "PHPSESSID": "", "tier": "pro_tier" };
 
-/// Fetches each proxy.
-fn get_proxies() -> Vec<String> {
-    reqwest::blocking::Client::new()
-        .get(PROXIES_URL)
-        .send()
-        .expect("unable to grab proxies")
-        .text()
-        .expect("invalid proxies")
-        .lines()
-        .map(|x| x.to_string())
-        .collect()
-}
+async function reqHandler(req: Request) {
+    const RoProURL = new URL(req.url);
+    const FoundAPI = RoProURL.pathname.indexOf("///api");
+    RoProURL.host = FoundAPI != -1 ? "api.ropro.io" : "ropro.io";
+    if (FoundAPI != -1) RoProURL.pathname = RoProURL.pathname.substring(0, FoundAPI);
 
-/// Performs the entire patching process.
-fn patch(path: PathBuf, proxy: String) {
-    // The regex replace thing. We don't want to proxy everything, only the stuff that needs verification
-    let re = regex::Regex::new(r#"(https://api\.)ropro\.io/(validateUser\.php|getServerInfo\.php|getServerConnectionScore\.php|getServerAge\.php|getSubscription\.php)"#).unwrap();
-    let rep = format!("https://{}/${{2}}///api", proxy);
+    const CORSheaders = new Headers();
+    const origin = req.headers.get("origin") || "chrome-extension://adbacgifemdbhdkfppmeilbgppmhaobf";
 
-    // Patching the background file
-    let background = path.join("background.js");
-    let background_contents = fs::read_to_string(&background).expect("Unable to open file (background.js)");
-    let new_background_contents = re.replace_all(&background_contents, &rep).to_string();
-    fs::write(&background, new_background_contents.clone()).expect("Unable to write file contents (background.js)");
+    let AllowedHeaders = "";
+    for (const header of req.headers.keys()) {
+        AllowedHeaders += header + ", ";
+    }
+    AllowedHeaders += "ropro-id, ropro-verification";
 
-    // Checking if they changed
-    if background_contents == new_background_contents {
-        println!("warning: nothing changed while patching `background.js` (and possibly others within js/page) - already patched?");
+    CORSheaders.set("access-control-allow-origin", origin);
+    CORSheaders.set("access-control-allow-headers", AllowedHeaders);
+    CORSheaders.set("access-control-allow-credentials", "true");
+
+    if (req.method.toUpperCase() == "OPTIONS") {
+        return new Response(null, { headers: CORSheaders });
     }
 
-    // Patching each file in js/page
-    let jspage = path.join("js/page");
-    for dir_entry in fs::read_dir(jspage).unwrap() {
-        // Get the file path
-        let file = dir_entry.unwrap();
-        let file_name = format!("js/page/{}", file.file_name().to_str().unwrap());
-        let file_path = file.path();
-    
-        // Patch the file
-        let file_data = fs::read_to_string(file_path.clone()).unwrap_or_else(|_| panic!("Unable to open file ({})", file_name));
-        let new_file_data = re.replace_all(&file_data, &rep).to_string();
-        fs::write(file_path.clone(), new_file_data.clone()).unwrap_or_else(|_| panic!("Unable to write file contents ({})", file_name));
-    }
-}
-
-/// Downloads RoPro source.
-fn download_extension() -> Vec<u8> {
-    // Download the extension
-    let mut crx_query = ChromeCRXQuery::default();
-    crx_query.x = "adbacgifemdbhdkfppmeilbgppmhaobf";
-    let extension_crx = crx_query.download_blocking().unwrap();
-
-    // Convert it to .zip
-    let crx_zip = crx_to_zip(extension_crx, None).unwrap();
-
-    // Done
-    crx_zip
-}
-
-/// Downloads RoPro source, then output to file as `.zip`.
-fn download_extract() {
-    // Download the extension's source
-    let extension_source = download_extension();
-
-    // Output to file
-    let mut file_out = File::create(format!("{}.zip", "RoPro")).unwrap();
-    file_out.write_all(&extension_source).unwrap();
-
-    // Output
-    println!("Downloaded RoPro.");
-}
-
-/// Downloads RoPro source and patches automatically.
-fn download_patch(selected_proxy: String) {
-    // Download the extension's source
-    let extension_source = download_extension();
-
-    // Extract the extension
-    let extract_dir = PathBuf::from("RoPro");
-    zip_extract::extract(Cursor::new(extension_source), &extract_dir, true).unwrap();
-
-    // Patch
-    patch(extract_dir, selected_proxy.to_string());
-    println!("Finished patching.");
-}
-
-/// Entrypoint.
-fn main() {
-    // Grab all proxies
-    let proxies = get_proxies();
-
-    // Grab vars, checking if using automated process
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() == 2 {
-        // Figure out which proxy we are using
-        let arg = args.get(1).unwrap();
-        let selected_proxy = if arg.chars().next().unwrap().is_numeric() {
-            proxies
-                .get(arg.parse::<usize>().unwrap())
-                .expect("unable to get proxy index")
-        } else {
-            arg
-        };
-
-        // Download and patch
-        download_patch(selected_proxy.to_string());
-
-        // Zip up
-        let source_dir = PathBuf::from("RoPro");
-        zip_create_from_directory(&PathBuf::from("RoPro-PATCHED.zip"), &source_dir)
-            .expect("unable to create zip");
-
-        // Delete directory
-        fs::remove_dir_all(source_dir)
-            .expect("unable to remove RoPro folder");
-
-        // Done
-        return
+    if (RoProURL.pathname == "/getSubscription.php") {
+        return new Response(Data.tier, { status: 200, headers: CORSheaders });
     }
 
-    // Construct the menu and run it
-    let menu = menu(vec![
-        label("-------------------------"),
-        label("-     RoPro Patcher     -"),
-        label("- Created by Stefanuk12 -"),
-        label("-------------------------"),
-        submenu("Custom Patch", vec![
-            label      ("-----------------------------------"),
-            label      ("-     RoPro Patcher - Patcher     -"),
-            label      ("-      Created by Stefanuk12      -"),
-            label      ("-----------------------------------"),
-            scroll     ("Select a proxy", proxies.clone()),
-            string     ("Custom proxy (overwrites)", "", true),
-            label      ("--------------"),
-            string     ("RoPro Path", "./", false),
-            list       ("Use Opera GX Path", vec!["No", "Yes"]),
-            label      ("--------------"),
-            button     ("Start"),
-            back_button("Back")
-        ]),
-        button("Download RoPro source as .zip"),
-        button("Download and Patch (uses default proxy)"),
-        back_button("Exit")
-    ]);
-    run(&menu);
+    const headers = new Headers(req.headers);
+    if (Data.PHPSESSID != "") {
+        headers.set("Cookie", `PHPSESSID=${Data.PHPSESSID}`);
+    }
 
-    // User has exited, process their action
-    let mut mm = mut_menu(&menu);
-    let selected_item = mm.selected_item_name();
-    match selected_item {
-        "Exit" => return println!("Goodbye!"),
-        "Download RoPro source as .zip" => download_extract(),
-        "Download and Patch (uses default proxy)" => download_patch(proxies.get(1).unwrap().to_string()),
-        "Custom Patch" => {
-            // Grab their selected proxy
-            let patch_menu = mm.get_submenu("Custom Patch");
-            let custom_proxy = patch_menu.selection_value("Custom proxy (overwrites)");
-            let selected_proxy = if custom_proxy.is_empty() { patch_menu.selection_value("Select a proxy") } else { custom_proxy }; 
-            
-            // Grab their selected path
-            let selected_path = if patch_menu.selection_value("Use Opera GX Path") == "Yes" {
-                let ext_path = AppDirs::new(Some(r"Opera Software\Opera GX Stable\Extensions\adbacgifemdbhdkfppmeilbgppmhaobf"), false).unwrap().config_dir;
-                fs::read_dir(ext_path)
-                    .expect("extension not installed?")
-                    .flatten()
-                    .filter(|x| x.metadata().unwrap().is_dir())
-                    .max_by_key(|x| x.metadata().unwrap().modified().unwrap())
-                    .unwrap()
-                    .path()
-            } else {
-                PathBuf::from(patch_menu.selection_value("RoPro Path"))
-            };
+    const response = await fetch(RoProURL, {
+        method: req.method,
+        headers: headers,
+        body: req.body
+    });
 
-            // Patch
-            patch(selected_path, selected_proxy.to_string());
-            println!("Finished patching.");
-        }
-        _ => return println!("You should not be seeing this...")
-    };
+    const responseHeaders = new Headers(response.headers);
+    responseHeaders.set("access-control-allow-origin", origin);
+    responseHeaders.set("access-control-allow-headers", AllowedHeaders);
+    responseHeaders.set("access-control-allow-credentials", "true");
+
+    return new Response(response.body, {
+        headers: responseHeaders,
+        status: response.status,
+        statusText: response.statusText
+    });
 }
+
+serve(reqHandler, { port: 443 });
+
+async function AlertRoPro() {
+    await fetch(`https://api.ropro.io/handleRoProAlert.php?timestamp=${Math.round(Date.now() / 1000)}`, {
+        headers: { Cookie: `PHPSESSID=${Data.PHPSESSID}` }
+    }).catch(() => console.error("Unable to alert RoPro"));
+}
+
+(async () => {
+    while (true) {
+        Data = await (await fetch(DataFetchURL)).json();
+        console.info("Refreshed data", Data);
+        await AlertRoPro();
+        await sleep(300);
+    }
+})();
